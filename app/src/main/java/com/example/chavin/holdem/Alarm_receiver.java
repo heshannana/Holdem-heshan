e5d22099fb5b87aee7584ed393c98c7e24bd1880
Heshan Nanayakkara <heshannana@gmail.com>
2017-05-10 19:28:40 +0530

    alarm off is also fixed

--- app/src/main/java/com/example/chavin/holdem/Alarm_receiver.java
+++ app/src/main/java/com/example/chavin/holdem/Alarm_receiver.java
@@ -14,8 +14,16 @@ public class Alarm_receiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.e("we are in the receiver","yay");
 
+        //fetch extra strings from the intent
+        String get_your_string = intent.getExtras().getString("extra");
+
+        Log.e("what is the key", get_your_string);
+
         //create an intent to the ringtone service
         Intent service_intent = new Intent(context, RingtoneplayingService.class);
+
+        //pass the extra string from main activvity to the the ringtoneplayingservice
+        service_intent.putExtra("extra", get_your_string);
 
         //start the ringtone service
         context.startService(service_intent);
--- app/src/main/java/com/example/chavin/holdem/RingtoneplayingService.java
+++ app/src/main/java/com/example/chavin/holdem/RingtoneplayingService.java
@@ -15,6 +15,9 @@ import android.widget.Toast;
 public class RingtoneplayingService extends Service {
 
     MediaPlayer media_song;
+    int startId;
+    boolean isRunning;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -25,9 +28,77 @@ public class RingtoneplayingService extends Service {
 
         Log.i("Local service", "Received start id" + startId + ":" + intent);
 
-        //create as instance of the media player
-        media_song = MediaPlayer.create(this, R.raw.dove);
-        media_song.start();
+        //fetch the extra string values
+        String state = intent.getExtras().getString("extra");
+
+        //converts the extra strings from the intent
+        //to startIDs , values 0 or 1
+        assert state != null;
+        switch (state){
+            case "alarm on":
+                startId = 1;
+                break;
+            case "alarm off":
+                startId = 0;
+                break;
+            default:
+                startId = 0;
+                break;
+        }
+
+        //if else statements
+
+        //if theres no music playing and the user pressed "alarm on"
+        //music should start playing
+        if(!this.isRunning && startId == 1){
+
+            //create as instance of the media player
+            media_song = MediaPlayer.create(this, R.raw.dove);
+            //start the ring tone
+            media_song.start();
+
+            this.isRunning = true;
+            this.startId = 0;
+        }
+
+        //if there is music playing and the user pressed "alarm off"
+        //music should stop playing
+        else if (this.isRunning && startId == 0){
+            //stop the ting tone
+            media_song.stop();
+            media_song.reset();
+
+            this.isRunning = false;
+            this.startId = 0;
+
+        }
+
+        //these are if the user presses random buttons
+        //just to bug proof the app
+        //if there is no music playing and the user pressed "alarm off"
+        //do nothing
+        else if(!this.isRunning && startId== 0){
+
+            this.isRunning = false;
+            this.startId = 0;
+        }
+
+        //if there is music playing and the user pressed "alarm on"
+        //do nothing
+        else if (!this.isRunning && startId == 1){
+
+            this.isRunning = true;
+            this.startId = 1;
+        }
+
+        //cant think of anything else, just to catch the odd event
+        else{
+
+        }
+
+
+
+
 
         return START_NOT_STICKY;
     }
@@ -35,6 +106,9 @@ public class RingtoneplayingService extends Service {
     @Override
     public void onDestroy(){
         //tell the user that we stopped
-        Toast.makeText(this, "on Destroy called", Toast.LENGTH_SHORT).show();
+        Log.e("on destroyed called", "ta da");
+
+        super.onDestroy();
+        this.isRunning = false;
     }
 }
